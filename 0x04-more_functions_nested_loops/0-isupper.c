7341e5296d72864470f9cb702bb8ef85a011cf44
Glorious <innovativepotencial@gmail.com>
2022-10-21 12:46:02 -0700

    learn

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ctype.h>
+/*
+ *Write a function that checks for uppercase character
+ *return 0
+ */
+    int isupper(int c)
+ { 
+		    if (c>='A' && c<='Z')
+		         return 1;
+		    else
+			 return 0;
+}
