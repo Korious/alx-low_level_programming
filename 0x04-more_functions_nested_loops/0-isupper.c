d2f7b98c6d1a1ee4d46f7942bf6c26fc5dbd1a17
Korious <113524903+Korious@users.noreply.github.com>
2022-10-21 20:56:41 +0100

    learn

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,10 +4,10 @@
  *Write a function that checks for uppercase character
  *return 0
  */
-    int isupper ( int c )
- { 
-		    if (c>='A' && c<='Z')
-		         return (1);
-		    else
-			 return (0);
+int isupper ( int c )
+{ 
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
