2b1065a2f70c52db51a28b9c2c79d62a8d37eeda
Glorious <innovativepotencial@gmail.com>
2022-10-25 06:42:54 -0700

    str

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: identifier for char
+ * Return 0
+ *
+ */
+
+void _puts(char *str)
+{
+	char *str[] = "Free";
+
+	puts(*str);
+	return (0);
+
+}
